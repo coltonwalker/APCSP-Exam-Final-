ba5bdc89ccdd70e39c7372ab9f0459f885c06891
Cole Walker <coltonwalker@amdg.rockhursths.edu>
2017-04-28 11:29:53 -0500

    finished beach

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
-var urls = ["emptybeach.html","beachwithfpole.html"];
+var urls = ["emptybeach.html","beachwithfpole.html","died.html"];
 
 function randomWalk() {
     var url = urls[Math.floor(Math.random()*urls.length)];
     console.log(url);
     window.location = url; 
 }
+
+
+var fishurls = ["fpolecatch.html", "fpolenocatch.html"];
+
+function randomCatch() {
+    var urlf = fishurls[Math.floor(Math.random()*fishurls.length)];
+    window.location = urlf;
+}
+
